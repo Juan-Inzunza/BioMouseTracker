4c24f250f8b0c14b65ac256cc67cc69b0c198da3
Heidy Ramirez <64618581+heidyramirez64@users.noreply.github.com>
2020-06-19 21:59:41 -0700

    Bluetooth Module
    Collects data from 4 motion sensors and transmits it to the RPi via HC-05 using serial communication

---
+++ HC-05 collecting data.ino
@@ -0,0 +1,70 @@
+//Main arduino 1 file
+ 
+
+void setup() {
+ Serial.begin(9600);
+ pinMode(8, INPUT); // first sensor input
+ pinMode(7, INPUT); // second sensor input
+ pinMode(6, INPUT); // third sensor input
+ pinMode(5, INPUT); // fourth sensor input
+
+}
+
+void loop()
+{
+
+    int sensordata_1 = digitalRead(8); // read status and store it into variable
+     {
+          if(sensordata_1 == LOW)        //  CAGE 1
+          {
+            Serial.println("s1: 1");  // 
+          }
+          else
+          {
+            Serial.println("s1: 0");
+          }
+          delay(200);
+      
+    }
+    int sensordata_2 = digitalRead(7);
+     {
+          if(sensordata_2 == LOW)     //  CAGE 2
+          {
+            Serial.println("s2: 1");
+          }
+          else
+          {
+            Serial.println("s2: 0");
+          }
+          delay(200);
+          
+    }
+//    int sensordata_3 = digitalRead(6);
+//    {
+//          if(sensordata_3 == LOW)    //  CAGE 3
+//          {
+//            Serial.println(String(1));
+//          }
+//          else
+//          {
+//            Serial.println(String(0));
+//          }
+//          delay(200);
+//      
+//    }
+//    int sensordata_4 = digitalRead(5);
+//    {
+//          if(sensordata_4 == LOW)    //  CAGE 4
+//          {
+//            Serial.println(String(1));
+//          }
+//          else
+//          {
+//            Serial.println(String(0));
+//          }
+//          delay(200);
+//      
+//    }
+//  
+}
+ 
